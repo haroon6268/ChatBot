288b4599ab304c2e44bb2c7b3c0720fe79fe0dc0
haroon6268 <136369742+haroon6268@users.noreply.github.com>
2024-09-17 20:21:57 -0400

    Added new Origin

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def get_secret():
 
 
 app = Flask(__name__)
-CORS(app, resources={"/*": {"origins": "https://chatbot-frontend-indol.vercel.app"}})
+CORS(app, resources={"/*": {"origins": ["https://chatbot-frontend-indol.vercel.app", "http://localhost:5173/"]}})
 key = get_secret()
 print(key)
 if not key:
